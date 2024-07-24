3379783267b349d3fbceb54e5310ccf82372d65b
servet0 <servetacar540@gmail.com>
2024-07-24 22:44:31 +0300

    firstRow modeli uygulandı

--- gallery_con/base/admin.py
+++ gallery_con/base/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import firstRow
+
+admin.site.register(firstRow)
 
 # Register your models here.
--- gallery_con/base/views.py
+++ gallery_con/base/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect
+from .models import firstRow
 
 def home(request):
+    firstrows = firstRow.objects.all().order_by("-date")
+
+    context = {
+        'firstrows': firstrows,
+    }
     
-    return render(request, 'base/home.html')
+    return render(request, 'base/home.html', context)
 
 # Create your views here.
