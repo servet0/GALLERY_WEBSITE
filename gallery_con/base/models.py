73213d1144649dd330ce9136ef2ef905b869ce5a
servet0 <servetacar540@gmail.com>
2024-07-24 22:21:32 +0300

    firstRow modeli düzenlendi

--- gallery_con/base/models.py
+++ gallery_con/base/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
-class firstRow(models.model):
+class firstRow(models.Model):
     title = models.CharField(max_length=1000, null=True, blank=True)
     image = models.ImageField(null=True, blank=True, upload_to="firstRow")
     text = models.TextField(null=True, blank=True)
+
+    date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
     
     class Meta:
-        verbosa_name = ("firstRow")
-        verbosa_name_plural = ("İlk satırdaki resimler")
+        verbose_name = ("firstRow")
+        verbose_name_plural = ("İlk satırdaki resimler")
 
 # Create your models here.
