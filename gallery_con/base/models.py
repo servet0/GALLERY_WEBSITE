a495681a8591cae4bf55a89c3895c64281870d4a
servet0 <servetacar540@gmail.com>
2024-07-24 22:18:22 +0300

    firstRow modeli oluşturuldu

--- gallery_con/base/models.py
+++ gallery_con/base/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 
+class firstRow(models.model):
+    title = models.CharField(max_length=1000, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to="firstRow")
+    text = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbosa_name = ("firstRow")
+        verbosa_name_plural = ("İlk satırdaki resimler")
 
 # Create your models here.
